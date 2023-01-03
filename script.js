f43b1cdc8209a3aeafb2632af0096c8774f97a4c
Flaviana <flaviferraz@yahoo.com.br>
2023-01-03 14:47:37 -0300

    Projeto Calculadora de Media inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome="Flaviana"
+var notaDoPrimeiroBimestre= 9.456
+var notaDoSegundoBimestre = 7.789
+var notaDoTerceiroBimestre = 4.637
+var notaDoQuartoBimestre= 2.987
+var notaFinal=(notaDoPrimeiroBimestre + notaDoSegundoBimestre+notaDoTerceiroBimestre+notaDoQuartoBimestre)/4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vinda," + nome)
+console.log(notaFixada)
+
+//Revisão
+//Variáveis, strings, console.log, tofixed, operações matemáticas, concatenação
